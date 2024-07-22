256ea2402cc00419902ff6446e06f6c24ecdf13a
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-22 11:58:20 -0400

    Completed the app

---
+++ SeaCreatures/SeaCreature.swift
@@ -0,0 +1,14 @@
+//
+//  SeaCreature.swift
+//  SeaCreatures
+//
+//  Created by Jordan Haynes on 7/22/24.
+//
+
+import Foundation
+
+struct SeaCreature: Identifiable {
+    var name: String
+    var modelName: String
+    var id = UUID()
+}
