999f1548529070b5731027793d2f49ad0c53526d
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-22 11:10:50 -0400

    Initial Commit

---
+++ SeaCreatures/SeaCreaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaCreaturesApp.swift
+//  SeaCreatures
+//
+//  Created by Jordan Haynes on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SeaCreaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
